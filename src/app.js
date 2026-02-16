ab5ce16bac2fa6e0b1fe5a02a14d8d3cba2b83b2
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-02-16 22:45:51 +0800

    feat: implement deleting from database and code refactor

--- src/app.js
+++ src/app.js
@@ -40,8 +40,8 @@ app.get('/', async (req, res) => {
                     <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
                         <span class="item-text">${item.text}</span>
                         <div>
-                        <button data-item-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-                        <button class="delete-me btn btn-danger btn-sm">Delete</button>
+                        <button data-item-id="${item._id}" class="edit-button btn btn-secondary btn-sm mr-1">Edit</button>
+                        <button data-item-id="${item._id}" class="delete-button btn btn-danger btn-sm">Delete</button>
                         </div>
                     </li>`,
 					)
@@ -72,6 +72,16 @@ app.post('/update-item', async (req, res) => {
 			$set: { text },
 		},
 	);
+
+	res.send('Success');
+});
+
+app.post('/delete-item', async (req, res) => {
+	const { id } = req.body;
+
+	const formattedId = mongodb.getIdFormat(id);
+
+	await mongodb.itemsCollection.deleteOne({ _id: formattedId });
 
 	res.send('Success');
 });
--- src/public/index.js
+++ src/public/index.js
@@ -1,35 +1,46 @@
-const listGroup = document.querySelector('ul.list-group');
+const editButtons = document.querySelectorAll('.edit-button');
+const deleteButtons = document.querySelectorAll('.delete-button');
 
-listGroup.addEventListener('click', async (event) => {
-	const element = event.target;
-
-	try {
-		validateElement(element);
-
-		const listItem = element.parentElement.parentElement;
+editButtons.forEach((editButton) => {
+	editButton.addEventListener('click', async () => {
+		const listItem = editButton.parentElement.parentElement;
 		const span = listItem.querySelector('.item-text');
 
 		const userInput = prompt('Enter Your New Text', span.innerText);
-		const itemId = element.getAttribute('data-item-id');
+		const itemId = editButton.getAttribute('data-item-id');
 
-		validateInputText(userInput);
+		try {
+			validateInputText(userInput);
 
-		await axios.post('/update-item', { text: userInput, id: itemId });
+			await axios.post('/update-item', { text: userInput, id: itemId });
 
-		runOptimisticUpdate(span, userInput);
-	} catch (error) {
-		console.warn(error.message);
-	}
+			runOptimisticUpdate(span, userInput);
+		} catch (error) {
+			console.warn(error.message);
+		}
+	});
 });
 
-function validateElement(element) {
-	const editButtonClass = 'edit-me';
-	const isNotEditButton = !element.classList.contains(editButtonClass);
+deleteButtons.forEach((deleteButton) => {
+	deleteButton.addEventListener('click', async (event) => {
+		const listItem = deleteButton.parentElement.parentElement;
+		const itemId = deleteButton.getAttribute('data-item-id');
 
-	if (isNotEditButton) {
-		throw new Error('Element is not the edit button.');
-	}
-}
+		const userResponse = confirm(
+			'Do you really want to delete this item permanently?',
+		);
+
+		try {
+			validateConfirmation(userResponse);
+
+			await axios.post('/delete-item', { id: itemId });
+
+			runItemDeletion(listItem);
+		} catch (error) {
+			console.warn(error.message);
+		}
+	});
+});
 
 function validateInputText(inputText) {
 	const isInputEmpty = !inputText;
@@ -39,6 +50,16 @@ function validateInputText(inputText) {
 	}
 }
 
+function validateConfirmation(isItemDeleting) {
+	if (!isItemDeleting) {
+		throw new Error('Item deleting process cancelled.');
+	}
+}
+
 function runOptimisticUpdate(element, newText) {
 	element.innerText = newText;
+}
+
+function runItemDeletion(element) {
+	element.remove();
 }
