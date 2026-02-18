207013b4d2151e28c83a7512ddbfc400b45c3760
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-02-18 17:53:31 +0800

    refactor: combine all database URI variables

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,6 @@ const { MongoClient, ObjectId } = require('mongodb');
 
 const logger = require('../utils/chalkLogger.js');
 
-const { DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME } = process.env;
-
-const DATABASE_URI = `mongodb+srv://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@free-tutorial-cluster.xqk44sl.mongodb.net/${DATABASE_NAME}?appName=Free-Tutorial-Cluster&retryWrites=true&w=majority`;
 const DATABASE_COLLECTION_NAME = 'items';
 
 class Database {
@@ -14,7 +11,7 @@ class Database {
 
 	async connect() {
 		try {
-			const client = new MongoClient(DATABASE_URI);
+			const client = new MongoClient(process.env.DATABASE_URI);
 			await client.connect();
 
 			logger.success('Success! Database is Connected.');
